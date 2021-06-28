86155300c8dee2edb5b2b8aae28bd55e4a2347e7
Ung Tsu Han <ungtsuhan@gmail.com>
2021-06-28 16:56:49 +0800

    Lesson 02 - Odd Occurrences In Array

---
+++ Lesson 02 - Arrays/OddOccurrencesInArray_01.cs
@@ -0,0 +1,44 @@
+///////////////////////////////////////////////////////////////////////////////////
+//
+// --------------------------------------------------------------------------------
+// Report
+// --------------------------------------------------------------------------------
+// Date         : 28 July 2021
+// Total Score  : 66%
+// Task Score   : 66%
+// Correctness  : 100%
+// Performance  : 25%
+//	
+///////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+
+class Solution {
+    
+    public int solution(int[] A) {
+        
+        List<int> oddOccurrences = new List<int>();
+
+        foreach(var num in A)
+        {
+            int indexOfNumInOddOccurrences = oddOccurrences.IndexOf(num);
+            
+            if(indexOfNumInOddOccurrences != -1)
+            {
+                // Appear second time, cancel off the odd occurrences
+                oddOccurrences.RemoveAt(indexOfNumInOddOccurrences);
+            }
+            else
+            {
+                // Appear first time, add value to odd occurrences list
+                oddOccurrences.Add(num);
+            }
+        }
+
+        if(oddOccurrences.Count > 0)
+            return oddOccurrences[0];
+        else
+            return 0;
+    }
+}
---
+++ Lesson 02 - Arrays/OddOccurrencesInArray_02.cs
@@ -0,0 +1,34 @@
+///////////////////////////////////////////////////////////////////////////////////
+//
+// --------------------------------------------------------------------------------
+// Report
+// --------------------------------------------------------------------------------
+// Date         : 28 July 2021
+// Total Score  : 100%
+// Task Score   : 100%
+// Correctness  : 100%
+// Performance  : 100%
+//	
+///////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Linq;
+
+class Solution {
+    
+    public int solution(int[] A) {
+        var oddOccurrenceLists = A.GroupBy(x => x)
+            .Select(n => new
+            {
+                num = n.Key,
+                count = n.Count()
+            })
+            .Where(u => (u.count %2) != 0)
+            .ToList();
+
+        if(oddOccurrenceLists.Count > 0)
+            return oddOccurrenceLists[0].num;
+        else
+            return 0;
+    }
+}   
