b372484a6568390c6f18868bbdf832ef8309f75f
Ung Tsu Han <ungtsuhan@gmail.com>
2021-06-29 20:21:19 +0800

    Lesson 03 - Perm Missing Elem

---
+++ Lesson 03 - Time Complexity/PermMissingElem_02.cs
@@ -0,0 +1,46 @@
+///////////////////////////////////////////////////////////////////////////////////
+//
+// --------------------------------------------------------------------------------
+// Report
+// --------------------------------------------------------------------------------
+// Date         : 29 July 2021
+// Total Score  : 100%
+// Task Score   : 100%
+// Correctness  : 100%
+// Performance  : 100%
+//	
+///////////////////////////////////////////////////////////////////////////////////
+
+using System;
+
+class Solution {
+    public int solution(int[] A) {
+        if(A.Length == 0)
+            return 1;
+
+        Array.Sort(A);
+        int largestNum = A[A.Length-1];
+        int smallestNum = A[0];
+        
+        if(largestNum - smallestNum == 0 || largestNum - smallestNum == (A.Length - 1))
+        {
+            if(smallestNum == 1)
+            {
+                return largestNum + 1;
+            }
+            else
+            {
+                return smallestNum - 1;
+            }
+        }
+        else
+        {
+            for(var i = 0; i < A.Length; i++)
+            {
+                if(A[i] != smallestNum+i)
+                    return smallestNum+i;
+            }
+            return 1;
+        }
+    }
+}
